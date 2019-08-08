5e9923b03a22a5cd147d17b5e2587c5e94a3c587
irzelindo salvador <irzelindo.salvador@gmail.com>
2019-08-08 18:48:07 +0200

    Add categories and questions endpoints

--- projects/02_trivia_api/starter/backend/flaskr/__init__.py
+++ projects/02_trivia_api/starter/backend/flaskr/__init__.py
@@ -1,5 +1,5 @@
 import os
-from flask import Flask, request, abort, jsonify
+from flask import Flask, request, abort, jsonify, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
 import random
@@ -7,25 +7,94 @@ import random
 from models import setup_db, Question, Category
 
 QUESTIONS_PER_PAGE = 10
+CATEGORIES_PER_PAGE = 5
+
+def questios_per_page(request, selection):
+  page = request.args.get('page', 1, type=int)
+  start = (page - 1) * QUESTIONS_PER_PAGE
+  end = QUESTIONS_PER_PAGE * page
+  questions = [question.format() for question in selection]
+  current_questions = questions[start:end]
+  return current_questions
+
+
+def categories_per_page(request, selection):
+  page = request.args.get('page', 1, type=int)
+  start = (page - 1) * CATEGORIES_PER_PAGE
+  end = CATEGORIES_PER_PAGE * page
+  categories = [category.format() for category in selection]
+  current_categories = categories[start:end]
+  return current_categories
+
 
 def create_app(test_config=None):
   # create and configure the app
   app = Flask(__name__)
   setup_db(app)
-  
   '''
   @TODO: Set up CORS. Allow '*' for origins. Delete the sample route after completing the TODOs
   '''
-
+  CORS(app)
   '''
   @TODO: Use the after_request decorator to set Access-Control-Allow
   '''
+  # CORS Headers 
+  @app.after_request
+  def after_request(response):
+    response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization,true')
+    response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
+    return response
+  '''
+  @DEFINING ERROR handlers functions
+  '''
+  @app.errorhandler(404)
+  def not_found(error):
+    return jsonify({
+        "success": False, 
+        "error": 404,
+        "message": "Resource Not found"
+        }), 404
+        
+  @app.errorhandler(400)
+  def bad_request(error):
+    return jsonify({
+        "success": False, 
+        "error": 400,
+        "message": "Bad Request"
+        }), 400
+
+  @app.errorhandler(422)
+  def unprocessable_entity(error):
+    return jsonify({
+        "success": False, 
+        "error": 422,
+        "message": "Unprocessable Request"
+        }), 422
 
   '''
   @TODO: 
   Create an endpoint to handle GET requests 
   for all available categories.
   '''
+  @app.route('/categories')
+  def get_categories():
+    try:
+      selection = Category.query.order_by(Category.id).all()
+      categories = categories_per_page(request, selection)
+
+      if len(categories) == 0:
+            abort(404)
+      else: 
+        return jsonify({
+          'success': True,
+          'response': 200,
+          'response_message': 'OK',
+          'categories': categories,
+          'total_categories': len(selection)
+        })
+
+    except:
+      abort(404)  
 
 
   '''
@@ -34,7 +103,33 @@ def create_app(test_config=None):
   including pagination (every 10 questions). 
   This endpoint should return a list of questions, 
   number of total questions, current category, categories. 
+  '''
 
+  @app.route('/questions')
+  def get_questions():
+    try:
+      selection = Question.query.order_by(Question.id).all()
+      questions = questios_per_page(request, selection)
+      selection = Category.query.order_by(Category.id).all()
+      categories = [category.type for category in selection]
+
+      if len(questions) == 0:
+            abort(404)
+      else: 
+        return jsonify({
+          'success': True,
+          'response': 200,
+          'response_message': 'OK',
+          'questions': questions,
+          'total_questions': len(selection),
+          'current_category': 4,
+          'categories': categories
+        })
+
+    except:
+      abort(404)  
+
+  '''
   TEST: At this point, when you start the application
   you should see questions and categories generated,
   ten questions per page and pagination at the bottom of the screen for three pages.
