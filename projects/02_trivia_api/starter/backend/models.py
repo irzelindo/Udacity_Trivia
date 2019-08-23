e5d234e57daa430a6d576805ea4c5a84846b0cbd
irzelindo salvador <irzelindo.salvador@gmail.com>
2019-08-23 12:43:28 +0200

    Conform  models.py with pep8

--- projects/02_trivia_api/starter/backend/models.py
+++ projects/02_trivia_api/starter/backend/models.py
@@ -1,78 +1,95 @@
-import os
-from sqlalchemy import Column, String, Integer, create_engine
+'''
+  This file contains all the database connection definition,
+  and all the CRUD for category and question tables.
+'''
+#import os
+#import json
+from sqlalchemy import Column, String, Integer
 from flask_sqlalchemy import SQLAlchemy
-import json
 
-database_name = "trivia"
-# database_path = "postgres://{}/{}".format('localhost:5432', database_name)
-database_path ="postgres://{}:{}@{}/{}".format('postgres', 'postgres','localhost:5432', database_name)
-db = SQLAlchemy()
+DATABASE_NAME = "trivia"
+# DATABASE_PATH = "postgres://{}/{}".format('localhost:5432', DATABASE_NAME)
+DATABASE_PATH = "postgres://{}:{}@{}/{}".format(
+    'postgres', 'postgres', 'localhost:5432', DATABASE_NAME)
+DB = SQLAlchemy()
 
 '''
 setup_db(app)
     binds a flask application and a SQLAlchemy service
 '''
-def setup_db(app, database_path=database_path):
+
+
+def setup_db(app, database_path=DATABASE_PATH):
+    ''' Database setup '''
     app.config["SQLALCHEMY_DATABASE_URI"] = database_path
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-    db.app = app
-    db.init_app(app)
-    db.create_all()
+    DB.app = app
+    DB.init_app(app)
+    DB.create_all()
 
-'''
-Question
 
-'''
-class Question(db.Model):  
-  __tablename__ = 'questions'
 
-  id = Column(Integer, primary_key=True)
-  question = Column(String)
-  answer = Column(String)
-  category = Column(String)
-  difficulty = Column(Integer)
+# Question
 
-  def __init__(self, question, answer, category, difficulty):
-    self.question = question
-    self.answer = answer
-    self.category = category
-    self.difficulty = difficulty
 
-  def insert(self):
-    db.session.add(self)
-    db.session.commit()
-  
-  def update(self):
-    db.session.commit()
+class Question(DB.Model):
+    ''' Question table definition '''
+    __tablename__ = 'questions'
 
-  def delete(self):
-    db.session.delete(self)
-    db.session.commit()
+    id = Column(Integer, primary_key=True)
+    question = Column(String)
+    answer = Column(String)
+    category = Column(String)
+    difficulty = Column(Integer)
 
-  def format(self):
-    return {
-      'id': self.id,
-      'question': self.question,
-      'answer': self.answer,
-      'category': self.category,
-      'difficulty': self.difficulty
-    }
+    def __init__(self, question, answer, category, difficulty):
+        self.question = question
+        self.answer = answer
+        self.category = category
+        self.difficulty = difficulty
 
-'''
-Category
+    def insert(self):
+        ''' Insert data into Question table '''
+        DB.session.add(self)
+        DB.session.commit()
 
-'''
-class Category(db.Model):  
-  __tablename__ = 'categories'
+    def update(self):
+        ''' Update data on Question table'''
+        DB.session.commit()
 
-  id = Column(Integer, primary_key=True)
-  type = Column(String)
+    def delete(self):
+        ''' Delete data on Questino table'''
+        DB.session.delete(self)
+        DB.session.commit()
 
-  def __init__(self, type):
-    self.type = type
+    def format(self):
+        ''' Serialize Question table data for json object '''
+        return {
+            'id': self.id,
+            'question': self.question,
+            'answer': self.answer,
+            'category': self.category,
+            'difficulty': self.difficulty
+        }
 
-  def format(self):
-    return {
-      'id': self.id,
-      'type': self.type
-    }
+
+# Category
+
+
+class Category(DB.Model):
+    ''' Category table definition '''
+    __tablename__ = 'categories'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+
+    def __init__(self, name):
+        self.name = name
+
+    def format(self):
+        ''' Serialize CAtegory table data for json object '''
+        return {
+            'id': self.id,
+            'name': self.name
+        }
+        
